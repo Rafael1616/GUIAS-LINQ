dc9b72f1a7dcb12ced60f63f6c657703e6166b96
juego <sd0058032023@unab.edu.sv>
2024-09-05 20:09:14 -0600

    Clausula First y FirstOrDefault

--- IntroduccionLinq/Program.cs
+++ IntroduccionLinq/Program.cs
@@ -159,6 +159,32 @@
             }
             Console.WriteLine(h1.datosHabitante());
             #endregion
+
+            #region FirsthAndFirsthOrDefault
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+            var primeraCasa = ListaCasas.First(); //esto no es linQ es  una fucnin de los Ienumarable
+            Console.WriteLine(primeraCasa.dameDatosCasa());
+
+            //aplicando una restriccion sin restricion lambda
+            Habitante personaEdad = (from variableTemporalHabitante in ListaHabitantes where variableTemporalHabitante.Edad > 25 select variableTemporalHabitante).First();
+            Console.WriteLine(personaEdad.datosHabitante());
+            Console.WriteLine("---------------------------Lo mismo pero con lambdas---------------------------------------------------------");
+            var Habitante1 = ListaHabitantes.First(objectTemp => objectTemp.Edad >25);
+            Console.WriteLine(Habitante1.datosHabitante());
+
+            // Si no tenemos el elemento que buscamos entoences la sonsulta devolvera una exepcion esto detendra el codigo en su totalidad
+
+            //Casa EdadError = (from vCasaTemp in ListaCasas where vCasaTemp.Id >10 select vCasaTemp).First() ;
+            //Console.WriteLine(EdadError.dameDatosCasa());
+
+            Casa CasaConFirsthOrDedault = ListaCasas.FirstOrDefault(vCasa => vCasa.Id > 200);
+            if (CasaConFirsthOrDedault == null ) {
+                Console.WriteLine("No existe !No hay!");
+                return;
+            }
+            Console.WriteLine("existe !Si existe!");
+            
+            #endregion
         }
     }
 }
